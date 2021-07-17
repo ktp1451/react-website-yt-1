8ce5c06719802bdb94b26b7e218c7b6928dcb42c
ktp1451 <76793752+ktp1451@users.noreply.github.com>
2021-07-16 21:54:20 -0500

    updated code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import expand from "emmet";
 import { Button } from "./Button";
+import "./Navbar.css";
 
 function NavBar() {
   const [click, setClick] = useState(false);
@@ -11,20 +12,24 @@ function NavBar() {
   const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
-    if(window.innerWidth <= 960) {
+    if (window.innerWidth <= 960) {
       setButton(false);
     } else {
       setButton(true);
     }
-  }
+  };
 
-  window.addEventListener('resize', showButton);
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             TRVL <i className="fab fa-typo3" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
@@ -64,7 +69,7 @@ function NavBar() {
               </Link>
             </li>
           </ul>
-          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
